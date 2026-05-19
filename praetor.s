fbf46ae2aa6e162b80c889637255d1c966e8f014
Augustin Mattei <augustin.mattei@epitech.eu>
2026-05-19 19:49:35 +0200

    feat: praetor now survives long enough to populate the whole mem with zjmps, wins 1v1 against any test opponents, even grouped

--- praetor.s
+++ praetor.s
@@ -3,15 +3,17 @@
 
     sti r1, %:defenses, %1
     sti r1, %:maykrtek, %1
+    sti r1, %:burn, %6
     fork %:defenses
+    fork %:ardent
 
     ld %1, r2
     ld %5, r3
-    ld %31, r4
+    ld %36, r4
 
 build:  sti r1, %:wall, r2
         sub r4, r2, r5
-        zjmp %:wall
+        zjmp %:maykrtek
         add r2, r3, r2
         ld %0, r16
         zjmp %:build
@@ -33,10 +35,20 @@ wall: live %0
     live %0
     live %0
     live %0
+defenses:   live %0
     ld %0, r16
     zjmp %:wall
 
-defenses:   live %0
-iwillnotdie:    fork %:defenses
-    ld %0, r16
-    zjmp %:defenses
+ardent: ldi %:burn, %1, r2
+        ld %9, r3
+        ld %5, r4
+        ld %5400, r6
+burn:   st r3, 200
+        live %666
+        add r2, r4, r2
+        add r4, r8, r8
+        sti r2, %:burn, %1
+        sub r6, r8, r9
+        zjmp %:wall
+        ld %0, r7
+        zjmp %:burn
